fe6efc99e6148d3667e80c2ba69a1997865e8f06
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-02-03 19:03:04 +0000

    bumped to version 0.12

--- lib/kerosene/version.rb
+++ lib/kerosene/version.rb
@@ -5,5 +5,5 @@ module Kerosene
   RUBY_VERSION =  IO
                   .read("#{File.dirname(__FILE__)}/../../.ruby-version")
                   .strip
-  VERSION = '0.1.0'
+  VERSION = '0.1.2'
 end
