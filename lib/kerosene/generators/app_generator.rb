bb80264f509f357f197cbf2ab40dd9d1f2f12320
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-02-04 22:19:32 +0000

    runnign webpacker install

--- lib/kerosene/generators/app_generator.rb
+++ lib/kerosene/generators/app_generator.rb
@@ -7,12 +7,25 @@ module Kerosene
   class AppGenerator < Rails::Generators::AppGenerator
     hide!
 
+    class_option :database, type: :string, aliases: "-d", default: "postgresql",
+      desc: "Configure for selected database (options: #{DATABASES.join("/")})"
+
     class_option  :github, type: :string, default: nil,
                   desc: 'Create Github repository and add remote origin pointed to repo'
 
+    class_option  :version, type: :boolean, aliases: "-v", group: :kerosene,
+                  desc: "Show Kerosene version number and quit"
+
+    class_option :help, type: :boolean, aliases: '-h', group: :kerosene,
+                  desc: "Show this help message and quit"
+
     class_option :skip_test, type: :boolean, default: true, desc: 'Skip Test Unit'
 
-    class_option :skip_turbolinks, type: :boolean, default: true, desc: 'Skip turbolinks gem'
+    class_option :skip_system_test,
+                 type: :boolean, default: true, desc: "Skip system test files"
+
+    class_option :skip_webpack_install, type: :boolean, default: false,
+                                          desc: "Don't run Webpack install"
 
     def finish_template
       invoke :kerosene_customization
@@ -45,10 +58,23 @@ module Kerosene
       say 'Congratulations! You\'ve just ignited rails.'
     end
 
+    def run_webpack
+      if webpack_install?
+        rails_command "webpacker:install"
+        rails_command "webpacker:install:#{options[:webpack]}" if options[:webpack] && options[:webpack] != "webpack"
+      end
+    end
+
     protected
 
     def get_builder_class
       Kerosene::AppBuilder
+    end
+
+    private
+
+    def webpack_install?
+      !(options[:skip_javascript] || options[:skip_webpack_install])
     end
   end
 end
